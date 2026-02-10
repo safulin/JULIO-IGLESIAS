a96ace70149388f9c58d6aff8201e9ba1abe901f
izansafu <izansantiago@elpuig.xeill.net>
2026-02-10 17:00:18 +0100

    first commit

---
+++ rosario-flores/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rosario-flores',
+  webDir: 'dist'
+};
+
+export default config;
